13ba75b107acd6f1ffde95d42bd46fbf8fdedd80
jjrv <juha@busfaster.com>
2017-10-13 20:37:16 +0300

    Optimize encode/decode and fix numbers over 32 bits.

--- src/index.ts
+++ src/index.ts
@@ -27,22 +27,6 @@ export function decodeSign(x: number) {
 	return((x >>> 1) ^ -(x & 1));
 }
 
-// Codec alphabet is all printable ASCII supported in JSON strings without escaping.
-// Space character is removed to avoid trailing spaces getting truncated somewhere.
-//                         1         2         3         4         5         6         7         8         9
-//                12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012
-const alphabet = "!#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~";
-const encodeTbl: number[] = [];
-const decodeTbl: number[] = [];
-
-for(let pos = 0; pos < alphabet.length; ++pos) {
-	const code = alphabet.charCodeAt(pos);
-	encodeTbl[pos] = code;
-	decodeTbl[code] = pos;
-}
-
-const extra = alphabet.length - 64;
-
 /** Variable length integer encoder. Output is an ASCII string, which never
   * gets changed by JSON.stringify (it only adds enclosing quotes). */
 
@@ -50,19 +34,26 @@ export class Writer {
 
 	static small(data: number[]) {
 		let count = data.length;
-		let code: number;
+		let ascii: number;
+		let digit: number;
 		let num: number;
 		let result = '';
 
 		while(count) {
 			num = data[--count];
-			result = encodeTbl[num & 63] + result;
-			num >>= 6;
+			digit = num % 64;
+			num = (num - digit) / 64;
+
+			ascii = digit + (((digit + 1934) * 9) >> 9);
+			result = String.fromCharCode(ascii) + result;
 
 			while(num) {
-				code = num % extra;
-				num = (num - code) / extra;
-				result = encodeTbl[code + 64] + result;
+				digit = num % 28;
+				num = (num - digit) / 28;
+
+				digit += 64;
+				ascii = digit + (((digit + 1934) * 9) >> 9);
+				result = String.fromCharCode(ascii) + result;
 			}
 		}
 
@@ -71,19 +62,26 @@ export class Writer {
 
 	static large(data: number[]) {
 		let count = data.length;
-		let code: number;
+		let ascii: number;
+		let digit: number;
 		let num: number;
 		let result = '';
 
 		while(count) {
 			num = data[--count];
-			code = num % extra;
-			num = (num - code) / extra;
-			result = encodeTbl[code + 64] + result;
+			digit = num % 28;
+			num = (num - digit) / 28;
+
+			digit += 64;
+			ascii = digit + (((digit + 1934) * 9) >> 9);
+			result = String.fromCharCode(ascii) + result;
 
 			while(num) {
-				result = encodeTbl[num & 63] + result;
-				num >>= 6;
+				digit = num % 64;
+				num = (num - digit) / 64;
+
+				ascii = digit + (((digit + 1934) * 9) >> 9);
+				result = String.fromCharCode(ascii) + result;
 			}
 		}
 
@@ -96,62 +94,44 @@ export class Writer {
 
 export class Reader {
 
-	constructor(public data: string, pos = 0, len = data.length) {
-		this.pos = pos - 1;
-		this.len = len - 1;
-	}
+	constructor(public data: string, public pos = 0) {}
 
-	/** Read a sequence of variable length integers. Small numbers compress better.
-	  * @param result Output array, reusable between calls for speed.
-	  * @param count Number of values to decode. */
+	/** Read a variable length integer. Small numbers compress better.
+	  * @return Decoded integer. */
 
-	small(result: number[], count: number) {
+	small() {
 		const data = this.data;
-		const len = this.len;
 		let pos = this.pos;
-		let posOut = -1;
-		let code: number;
-		let num: number;
+		let ascii: number;
+		let digit: number;
+		let num = 0;
 
-		while(pos < len && posOut < count) {
-			num = 0;
-
-			while((code = decodeTbl[data.charCodeAt(++pos)]) >= 64) {
-				num = num * extra + code - 64;
-			}
-
-			result[++posOut] = (num << 6) + code;
+		while((ascii = data.charCodeAt(pos++), digit = ascii - (((ascii + 1900) * 9) >> 9)) >= 64) {
+			num = num * 28 + digit - 64;
 		}
 
 		this.pos = pos;
 
-		return(posOut);
+		return(num * 64 + digit);
 	}
 
-	large(result: number[], count: number) {
+	/** Read a variable length integer. Large numbers compress better.
+	  * @return Decoded integer. */
+
+	large() {
 		const data = this.data;
-		const len = this.len;
 		let pos = this.pos;
-		let posOut = -1;
-		let code: number;
-		let num: number;
+		let ascii: number;
+		let digit: number;
+		let num = 0;
 
-		while(pos < len && posOut < count) {
-			num = 0;
-
-			while((code = decodeTbl[data.charCodeAt(++pos)]) < 64) {
-				num = (num << 6) + code;
-			}
-
-			result[++posOut] = num * extra + code - 64;
+		while((ascii = data.charCodeAt(pos++), digit = ascii - (((ascii + 1900) * 9) >> 9)) < 64) {
+			num = num * 64 + digit;
 		}
 
 		this.pos = pos;
 
-		return(posOut);
+		return(num * 28 + digit - 64);
 	}
-
-	pos: number;
-	len: number;
 
 }
